ad98a0753f89e698e8690b1da9dbd40ed1bd14fc
Rex-Imman Robles <reximman03@gmail.com>
2022-11-19 13:50:09 +0800

    adding play command

--- assistant.py
+++ assistant.py
@@ -9,14 +9,26 @@ engine.setProperty("voice", voices[1].id)
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-try:
-    with sr.Microphone() as source:
-        print("listening....")
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if "karen" in command:
-            talk(command)
-            print(command)
-except:
-    pass
+    
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening....")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if "karen" in command:
+                command = command.replace("karen", "")
+                print(command)
+    except:
+        pass
+    return command
+
+def run_karen():
+    command = take_command()
+    print(command)
+    if "play" in command:
+        talk("playing")
+        print("playing")
+        
+run_karen()
