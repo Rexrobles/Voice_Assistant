61983d1c8625c491825f45941dfb38a6cf4d3408
Rex-Imman Robles <reximman03@gmail.com>
2022-11-19 12:44:57 +0800

    importing command pyttsx3

--- assistant.py
+++ assistant.py
@@ -1,10 +1,15 @@
 import speech_recognition as sr
- 
+import pyttsx3
+
 listener = sr.Recognizer()
+engine = pyttsx3.init()
 try:
     with sr.Microphone() as source:
+        print("listening....")
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if "jarvis" in command:
+            print(command)
 except:
     pass
