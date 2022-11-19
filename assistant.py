8993a0c6ca6461e77cdd9ca135982691146164d8
Rex-Imman Robles <reximman03@gmail.com>
2022-11-19 13:24:47 +0800

    making the assistant repeat what i say

--- assistant.py
+++ assistant.py
@@ -3,13 +3,20 @@ import pyttsx3
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print("listening....")
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
         command = command.lower()
-        if "jarvis" in command:
+        if "karen" in command:
+            talk(command)
             print(command)
 except:
     pass
